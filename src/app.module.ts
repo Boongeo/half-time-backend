9fc82547359413be1fa0d0857a61d97553189a44
jun9898 <nice1998@gmail.com>
2024-11-15 13:27:58 +0900

    feat: project config파일 작성 - Swagger.config 작성 완료 - postgres.config 작성 완료   - docker-compose와 docker 파일 작성해야함 - 기본 logger winston logger로 변경 완료 - typeORM 설정 완료

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,47 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import postgresConfig from './config/postgres.config';
+import swaggerConfig from './config/swagger.config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { WinstonModule } from 'nest-winston';
+import loggerConfig from './config/logger.config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [postgresConfig, swaggerConfig, loggerConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        const isDev = configService.get('NODE_ENV') === 'development';
+        const obj: TypeOrmModuleOptions = {
+          type: 'postgres',
+          host: configService.get('postgres.host'),
+          port: configService.get('postgres.port'),
+          database: configService.get('postgres.database'),
+          username: configService.get('postgres.username'),
+          password: configService.get('postgres.password'),
+          autoLoadEntities: true,
+          synchronize: false,
+          logging: isDev,
+        };
+        if (isDev) console.log('Sync postgres with logging enabled');
+        return obj;
+      },
+    }),
+    WinstonModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const loggerConfig = configService.get('logger');
+        return {
+          transports: loggerConfig.transports,
+        };
+      },
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
