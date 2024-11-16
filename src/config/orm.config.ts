76c6c770b57d184a4c183e026c550fe7890c8770
jun9898 <nice1998@gmail.com>
2024-11-16 16:53:00 +0900

    feat: docker 환경설정 완료

--- src/config/orm.config.ts
+++ src/config/orm.config.ts
@@ -1,4 +1,9 @@
 import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+
+const envFile =
+  process.env.NODE_ENV === 'production' ? '.env.production' : '.env.local';
+dotenv.config({ path: envFile });
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
--- src/config/postgres.config.ts
+++ src/config/postgres.config.ts
@@ -3,7 +3,7 @@ import { registerAs } from '@nestjs/config';
 export default registerAs('postgres', () => ({
   host: process.env.POSTGRES_HOST,
   port: process.env.POSTGRES_PORT,
-  database: process.env.POSTGRES_DB,
+  database: process.env.POSTGRES_DATABASE,
   username: process.env.POSTGRES_USERNAME,
   password: process.env.POSTGRES_PASSWORD,
 }));
