5951507048259052cf4dbb83346f34ace39943cc
jun9898 <nice1998@gmail.com>
2024-11-18 22:55:06 +0900

    fix(mail): nodemailer -> MailerModule 변경

--- src/config/mail.config.ts
+++ src/config/mail.config.ts
@@ -1,8 +1,10 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('mail', () => ({
-  service: process.env.MAIL_SERVICE,
+  service: process.env.MAIL_SERVICE || 'gmail',
+  host: process.env.MAIL_HOST || 'smtp.gmail.com',
+  port: parseInt(process.env.MAIL_PORT, 10) || 587,
   user: process.env.MAIL_USER,
   password: process.env.MAIL_PASSWORD,
-  from: '"Half-time" <no-reply@gmail.com>',
+  from: `"Half-time" <${process.env.MAIL_USER}>`,
 }));
--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,33 +1,19 @@
 import { Injectable } from '@nestjs/common';
-import * as nodemailer from 'nodemailer';
-import { ConfigService } from '@nestjs/config';
-import Mail from 'nodemailer/lib/mailer';
-import { configureTemplateEngine } from './util/template-engine';
+import { MailerService } from '@nestjs-modules/mailer';
 
 @Injectable()
 export class MailService {
-  private transporter: Mail;
-
-  constructor(private readonly configService: ConfigService) {
-    this.transporter = nodemailer.createTransport({
-      service: this.configService.get<string>('mail.service'),
-      auth: {
-        user: this.configService.get<string>('mail.user'),
-        pass: this.configService.get<string>('mail.password'),
-      },
-    });
-    this.transporter.use('compile', configureTemplateEngine());
-  }
+  constructor(private readonly mailerService: MailerService) {}
 
   async sendVerifyToken(email: string, verifyToken: number) {
     const mailOptions = {
       to: email,
       subject: '[Half-time] 이메일 인증',
-      template: 'verify-email',
+      template: './verify-email',
       context: {
         verifyToken,
       },
     };
-    return await this.transporter.sendMail(mailOptions);
+    return await this.mailerService.sendMail(mailOptions);
   }
 }
