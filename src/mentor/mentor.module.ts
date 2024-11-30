a183613e7287e7fd202201103aa96bc393de042c
jun9898 <nice1998@gmail.com>
2024-11-30 13:57:40 +0900

    feat(mentor): Mentor controller, service 파일 추가

--- src/mentor/mentor.module.ts
+++ src/mentor/mentor.module.ts
@@ -3,13 +3,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Mentor } from './entity/mentor.entity';
 import { MentorTechStack } from './entity/mentor-tech-stack.entity';
 import { MentorInterest } from './entity/mentor-interest.entity';
+import { MentorController } from './mentor.controller';
+import { MentorService } from './mentor.service';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Mentor, MentorTechStack, MentorInterest]),
   ],
   exports: [],
-  controllers: [],
-  providers: [],
+  controllers: [MentorController],
+  providers: [MentorService],
 })
 export class MentorModule {}
