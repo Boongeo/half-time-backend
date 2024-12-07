7ab39b41ac71449936936bd6cc2b0ed573794b78
jun9898 <nice1998@gmail.com>
2024-12-07 20:20:13 +0900

    refactor(mentor): 요구사항에 맞게 멘토 Entity 컬럼 추가

--- src/mentor/entity/mentor.entity.ts
+++ src/mentor/entity/mentor.entity.ts
@@ -3,7 +3,7 @@ import { BaseEntity } from '../../common/entity/base.entity';
 import { User } from '../../user/entity/user.entity';
 import { MentorTechStack } from './mentor-tech-stack.entity';
 import { MentorInterest } from './mentor-interest.entity';
-import { MentorAccept } from '../enum/mentor.enum';
+import { MentorAccept, MentoringType } from '../enum/mentor.enum';
 
 @Entity()
 export class Mentor extends BaseEntity {
@@ -29,4 +29,25 @@ export class Mentor extends BaseEntity {
 
   @OneToMany(() => MentorInterest, (mentorInterest) => mentorInterest.mentor)
   interests: MentorInterest[];
+
+  @Column()
+  company: string;
+
+  @Column()
+  experience: number;
+
+  @Column()
+  hourlyRate: number;
+
+  @Column({
+    type: 'enum',
+    enum: MentoringType,
+  })
+  mentoringType: MentoringType;
+
+  @Column()
+  preferredRegion: string;
+
+  @Column()
+  careerProofPath: string;
 }
