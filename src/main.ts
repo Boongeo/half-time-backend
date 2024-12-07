d9c2d5e993d4c771840f7c49e3422b95cd6a68fb
jun9898 <nice1998@gmail.com>
2024-12-07 20:21:36 +0900

    feat(mentor): 민감한 정보를 저장할 file path 추가

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,8 @@ import { initializeTransactionalContext } from 'typeorm-transactional';
 import * as session from 'express-session';
 import * as express from 'express';
 import { join } from 'path';
+import passport from 'passport';
+import { Role } from './user/enums/role.enum';
 
 async function bootstrap() {
   initializeTransactionalContext();
@@ -35,6 +37,21 @@ async function bootstrap() {
 
   const configService = app.get(ConfigService);
   app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
+  app.use(
+    '/sensitive-uploads',
+    (req, res, next) => {
+      passport.authenticate('jwt', { session: false }, (err, user, info) => {
+        if (err) return next(err);
+        if (!user || !user.roles.includes(Role.ADMIN)) {
+          return res.status(403).json({ message: '관리자 권한이 필요합니다.' });
+        }
+        req.user = user;
+        next();
+      })(req, res, next);
+    },
+    express.static(join(__dirname, '..', 'sensitive-uploads')),
+  );
+
   app.useLogger(WinstonModule.createLogger(configService.get('logger')));
   app.setGlobalPrefix('api');
   app.useGlobalPipes(
