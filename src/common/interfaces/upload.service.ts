4eac39d47e374a8069f79dd4a8716854f236fb8c
jun9898 <nice1998@gmail.com>
2024-12-07 20:29:07 +0900

    refactor(upload): 민감한 파일을 업로드하는 로직 분리

--- src/common/interfaces/upload.service.ts
+++ src/common/interfaces/upload.service.ts
@@ -1,7 +1,7 @@
 export interface UploadService {
   uploadFile(file: Express.Multer.File): Promise<string>;
 
-  async;
+  uploadSensitiveFile(file: Express.Multer.File): Promise<string>;
 
   updateFile(
     profileImage: Express.Multer.File,
